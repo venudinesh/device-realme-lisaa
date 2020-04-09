6302f8da1bbbb2b6aec5b265d021433b37bc1e7d
Roger Liao <rogerliao@google.com>
2020-04-10 04:46:38 +0800

    vitamin: Add vndservicemanager to the product
    vndservicemanager has been removed for the devices with API level
    >= 30 but Redbull still needs it to provide vendor-implemented services.
    
    Ref: b/149867896
    Bug: 148823278
    Change-Id: I08ed6a242c8ac1f64da12204d618376fe1a5c8ac

--- device.mk
+++ device.mk
@@ -362,6 +362,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
+# VNDK
+PRODUCT_PACKAGES += \
+    vndservicemanager
+
 # Wifi
 PRODUCT_PACKAGES += \
     wpa_supplicant \
