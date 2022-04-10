4fae6cf479e71bd4cf798fd09dcef90cd060d14e
LuK1337 <priv.luk@gmail.com>
2022-04-10 16:11:27 +0200

    vitamin: Build oplus vibrator HAL
    Change-Id: Iaeb72c78919e309f186a50fdf6b2fecba80f4abd

--- device.mk
+++ device.mk
@@ -391,6 +391,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service.oplus
+
+PRODUCT_COPY_FILES += \
+    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+
 # VNDK
 PRODUCT_PACKAGES += \
     vndservicemanager
