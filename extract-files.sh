3c84ce4e8c314442c07d00daf5290877f568419f
bengris32 <bengris32@protonmail.ch>
2023-12-08 23:56:33 +0000

    vitamin: Link mnld binary against `android.hardware.sensors@1.0-convert`

--- extract-files.sh
+++ extract-files.sh
@@ -62,7 +62,7 @@ function blob_fixup {
         vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b)
             "${PATCHELF}" --add-needed "libstagefright_foundation-v33.so" "${2}"
             ;;
-        vendor/lib64/hw/android.hardware.sensors@2.X-subhal-mediatek.so|vendor/lib64/mt6983/libaalservice.so)
+        vendor/bin/mnld|vendor/lib64/hw/android.hardware.sensors@2.X-subhal-mediatek.so|vendor/lib64/mt6983/libaalservice.so)
             "${PATCHELF}" --add-needed "libshim_sensors.so" "${2}"
             ;;
         vendor/lib64/hw/mt6983/vendor.mediatek.hardware.pq@2.15-impl.so)
