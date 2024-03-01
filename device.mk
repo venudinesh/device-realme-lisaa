60d3480170310e4a157e5bb0b3e728d204297ce1
bengris32 <bengris32@protonmail.ch>
2024-03-01 20:53:37 +0530

    vitamin: Build `MtkInCallService`

--- device.mk
+++ device.mk
@@ -71,6 +71,9 @@ PRODUCT_PACKAGES += \
     libtinycompress \
     libvisualizer
 
+PRODUCT_PACKAGES += \
+    MtkInCallService
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_ODM)/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
