0f65cea701db790651eeae5b9724b469208c4150
bengris32 <bengris32@protonmail.ch>
2024-03-10 14:51:58 +0000

    vitamin: Link main Audio HW module against V33 libstagefright_foundation
    Change-Id: Id2dfd90a671846fb0108503d642195eaa4a25f12
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- extract-files.sh
+++ extract-files.sh
@@ -71,6 +71,9 @@ function blob_fixup {
         vendor/bin/mnld|vendor/lib64/hw/android.hardware.sensors@2.X-subhal-mediatek.so|vendor/lib64/mt6983/libaalservice.so)
             "${PATCHELF}" --add-needed "libshim_sensors.so" "${2}"
             ;;
+        vendor/lib64/hw/audio.primary.mediatek.so)
+            "${PATCHELF}" --add-needed "libstagefright_foundation-v33.so" "${2}"
+            ;;
         vendor/lib64/hw/mt6983/vendor.mediatek.hardware.pq@2.15-impl.so)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
             "${PATCHELF}" --add-needed "libshim_sensors.so" "${2}"
