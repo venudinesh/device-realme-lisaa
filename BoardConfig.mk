39082cad8ffb045a7490c7ec6614d667ae6d1eec
bengris32 <bengris32@protonmail.ch>
2024-04-13 23:26:11 +0100

    vitamin: Inherit common MediaTek framework compatibility matrix
    Change-Id: I0a11f243c039c45091724e1b57d9bbcd1729f5f9
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,7 +26,9 @@ TARGET_NO_BOOTLOADER := true
 TARGET_SCREEN_DENSITY := 560
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/device_framework_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(DEVICE_PATH)/device_framework_matrix.xml \
+    hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE += $(DEVICE_PATH)/compatibility_matrix.xml
 
