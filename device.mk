01263d5349e4e1ea180e20eba4db4532f62e794e
Mashopy <eliasgheeraert@gmail.com>
2024-04-23 13:09:34 +0200

    vitamin: Add `hardware/oplus` into Soong namespaces

--- device.mk
+++ device.mk
@@ -348,7 +348,8 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/mediatek
+    hardware/mediatek \
+    hardware/oplus
 
 # Telephony
 PRODUCT_COPY_FILES += \
