2c4fab0f7d882c72cf787bcd53b8e643a7cc4150
Mashopy <eliasgheeraert@gmail.com>
2024-09-22 21:56:08 +0200

    vitamin: Make blob_fixup function more readable
    Basically reorder the whole section alphabetically and use columns

--- extract-files.sh
+++ extract-files.sh
@@ -73,17 +73,19 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "android.hardware.graphics.common-V2-ndk_platform.so" "android.hardware.graphics.common-V5-ndk.so" "${2}"
             ;;
+        odm/lib64/libCOppLceTonemapAPI.so|\
+        odm/lib64/libYTCommon.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
+            ;;
         odm/lib64/vendor.oplus.hardware.hdcp-V1-ndk_platform.so)
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "android.hardware.common-V2-ndk_platform.so" "android.hardware.common-V2-ndk.so" "${2}"
             ;;
-        odm/lib64/libCOppLceTonemapAPI.so|odm/lib64/libYTCommon.so)
+        system_ext/etc/init/init.vtservice.rc|\
+        vendor/etc/init/android.hardware.neuralnetworks-shim-service-mtk.rc)
             [ "$2" = "" ] && return 0
-            "${PATCHELF}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
-            ;;
-        system_ext/priv-app/ImsService/ImsService.apk)
-            [ "$2" = "" ] && return 0
-            apktool_patch "${2}" "${MY_DIR}/blob-patches/ImsService.patch"
+            sed -i "s|start|enable|g" "${2}"
             ;;
         system_ext/lib64/libsink.so)
             [ "$2" = "" ] && return 0
@@ -93,19 +95,19 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             grep -q "libui_shim.so" "${2}" || "${PATCHELF}" --add-needed "libui_shim.so" "${2}"
             ;;
-        system_ext/etc/init/init.vtservice.rc|vendor/etc/init/android.hardware.neuralnetworks-shim-service-mtk.rc)
+        system_ext/priv-app/ImsService/ImsService.apk)
             [ "$2" = "" ] && return 0
-            sed -i "s|start|enable|g" "${2}"
-            ;;
-        vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b|vendor/lib64/hw/audio.primary.mediatek.so)
-            [ "$2" = "" ] && return 0
-            "${PATCHELF}" --replace-needed "libavservices_minijail_vendor.so" "libavservices_minijail.so" "${2}"
-            grep -q "libstagefright_foundation-v33.so" "${2}" || "${PATCHELF}" --add-needed "libstagefright_foundation-v33.so" "${2}"
+            apktool_patch "${2}" "${MY_DIR}/blob-patches/ImsService.patch"
             ;;
         vendor/bin/hw/android.hardware.gnss-service.mediatek|\
         vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so)
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "android.hardware.gnss-V1-ndk_platform.so" "android.hardware.gnss-V1-ndk.so" "${2}"
+            ;;
+        vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --replace-needed "libavservices_minijail_vendor.so" "libavservices_minijail.so" "${2}"
+            grep -q "libstagefright_foundation-v33.so" "${2}" || "${PATCHELF}" --add-needed "libstagefright_foundation-v33.so" "${2}"
             ;;
         vendor/bin/hw/android.hardware.security.keymint-service.trustonic)
             [ "$2" = "" ] && return 0
@@ -114,7 +116,10 @@ function blob_fixup() {
             "${PATCHELF}" --replace-needed "android.hardware.security.secureclock-V1-ndk_platform.so" "android.hardware.security.secureclock-V1-ndk.so" "${2}"
             grep -q "android.hardware.security.rkp-V3-ndk.so" "${2}" || "${PATCHELF}" --add-needed "android.hardware.security.rkp-V3-ndk.so" "${2}"
             ;;
-        vendor/bin/mnld|vendor/lib64/hw/android.hardware.sensors@2.X-subhal-mediatek.so|vendor/lib64/liboplus_mtkcam_lightsensorprovider.so|vendor/lib64/mt6983/libaalservice.so)
+        vendor/bin/mnld|\
+        vendor/lib64/hw/android.hardware.sensors@2.X-subhal-mediatek.so|\
+        vendor/lib64/liboplus_mtkcam_lightsensorprovider.so|\
+        vendor/lib64/mt6983/libaalservice.so)
             [ "$2" = "" ] && return 0
             grep -q "libshim_sensors.so" "${2}" || "${PATCHELF}" --add-needed "libshim_sensors.so" "${2}"
             ;;
@@ -126,23 +131,19 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             sed -i '/<CamcorderProfiles cameraId="3">/,/<\/CamcorderProfiles>/ { /^[[:space:]]*<!--/! { /^[[:space:]]*$/! { s/^/<!-- /; s/$/ -->/; } } }' "${2}"
             ;;
-        vendor/lib64/hw/mt6983/vendor.mediatek.hardware.pq@2.15-impl.so)
+        vendor/lib64/hw/audio.primary.mediatek.so)
             [ "$2" = "" ] && return 0
-            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
-            grep -q "libshim_sensors.so" "${2}" || "${PATCHELF}" --add-needed "libshim_sensors.so" "${2}"
+            grep -q "libstagefright_foundation-v33.so" "${2}" || "${PATCHELF}" --add-needed "libstagefright_foundation-v33.so" "${2}"
             ;;
         vendor/lib64/hw/mt6983/android.hardware.camera.provider@2.6-impl-mediatek.so)
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
             grep -q "libcamera_metadata_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcamera_metadata_shim.so" "${2}"
             ;;
-        vendor/lib64/libmidasserviceintf_aidl.so)
+        vendor/lib64/hw/mt6983/vendor.mediatek.hardware.pq@2.15-impl.so)
             [ "$2" = "" ] && return 0
-            "${PATCHELF}" --replace-needed "android.frameworks.stats-V1-ndk_platform.so" "android.frameworks.stats-V1-ndk.so" "${2}"
-            ;;
-        vendor/lib64/mt6983/libmnl.so)
-            [ "$2" = "" ] && return 0
-            grep -q "libcutils.so" "${2}" || "${PATCHELF}" --add-needed "libcutils.so" "${2}"
+            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
+            grep -q "libshim_sensors.so" "${2}" || "${PATCHELF}" --add-needed "libshim_sensors.so" "${2}"
             ;;
         vendor/lib64/lib3a.ae.pipe.so|\
         vendor/lib64/mt6983/lib3a.awbsync.so|\
@@ -151,6 +152,14 @@ function blob_fixup() {
         vendor/lib64/mt6983/lib3a.sensors.flicker.so)
             [ "$2" = "" ] && return 0
             grep -q "liblog.so" "${2}" || "${PATCHELF_0_17_2}" --add-needed "liblog.so" "${2}"
+            ;;
+        vendor/lib64/libmidasserviceintf_aidl.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --replace-needed "android.frameworks.stats-V1-ndk_platform.so" "android.frameworks.stats-V1-ndk.so" "${2}"
+            ;;
+        vendor/lib64/mt6983/libmnl.so)
+            [ "$2" = "" ] && return 0
+            grep -q "libcutils.so" "${2}" || "${PATCHELF}" --add-needed "libcutils.so" "${2}"
             ;;
         vendor/lib64/mt6983/libmtkcam_stdutils.so)
             [ "$2" = "" ] && return 0
